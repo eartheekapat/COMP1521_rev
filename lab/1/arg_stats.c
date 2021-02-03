488ffb3b27492b00adfb5a74e08c847f22ec254e
eartheekapat <eartheekapat@gmail.com>
2021-02-03 14:27:30 +1100

    done with lab 1

--- lab/1/arg_stats.c
+++ lab/1/arg_stats.c
@@ -2,14 +2,36 @@
 #include <stdlib.h>
 
 int main(int argc, char **argv) {
-	printf("Program name: ./my_args\n");
-	if (argc == 1) {
-		printf("There are no other arguments");
-	} else {
-		printf("There are %d arguments\n", argc);
-		for (int i = 1; i <= argc; i++) {
-			printf("\tArgument %d is \"%s\"", i, argv[i]);
+	if (argc > 11) {
+		printf("Too many argument");
+	}
+	int min, max, sum, prod, mean;
+	for (int i = 1; i < argc; i++) {
+		// first loop assign the number to all 
+		int num = strtol(argv[i], NULL, 10);
+		if (i == 1) {
+			min = num;
+			max = num;
+			sum = num;
+			prod = num;
+			mean = num;
+		} else {
+			if (num < min) {
+				min = num;
+			}
+			if (num > max) {
+				max = num;
+			}
+			sum += num;
+			prod *= num;
+			mean = sum / (argc - 1);
 		}
 	}
+	printf("MIN:  %d\n", min);
+	printf("MAX:  %d\n", max);
+	printf("SUM:  %d\n", sum);
+	printf("PROD:  %d\n", prod);
+	printf("MEAN:  %d\n", mean);
+
 	return 0;
 }
